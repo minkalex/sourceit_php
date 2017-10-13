3c1e649f697e0f86db2a40b7b32bba886d0e842b
MINK <sasha-minko@mail.ru>
2017-10-13 17:44:02 +0300

    hw11

---
+++ mink_hw11/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO `messages`(`message_id`, `created`, `username`, `message`) VALUES (NULL, "2017-10-12 12:20:23", "asdasdasd", "asd");
+INSERT INTO `messages`(`message_id`, `created`, `username`, `message`) VALUES (NULL, "2017-10-12 12:21:12", "asd", "asdasdasda");
+INSERT INTO `messages`(`message_id`, `created`, `username`, `message`) VALUES (NULL, "2017-10-12 12:25:33", "a", "ad");
+INSERT INTO `messages`(`message_id`, `created`, `username`, `message`) VALUES (NULL, "2017-10-12 12:36:52", "dsf", "sdfsdfsd");
+INSERT INTO `messages`(`message_id`, `created`, `username`, `message`) VALUES (NULL, "2017-10-12 12:38:32", "1313", "sdfsdfdfsdf");
+INSERT INTO `messages`(`message_id`, `created`, `username`, `message`) VALUES (NULL, "2017-10-12 12:40:55", "qwe", "qweqweqwe");
+INSERT INTO `messages`(`message_id`, `created`, `username`, `message`) VALUES (NULL, "2017-10-12 12:44:03", "12", "121212");
+INSERT INTO `messages`(`message_id`, `created`, `username`, `message`) VALUES (NULL, "2017-10-12 12:45:59", "123", "12312312312");
+INSERT INTO `messages`(`message_id`, `created`, `username`, `message`) VALUES (NULL, "2017-10-12 12:46:20", "qeqwqw", "qweqweqweqweqwe");
